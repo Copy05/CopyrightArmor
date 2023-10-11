e9a4b4f02539108e8d86a05bb9724fb8dcc1dd3e
Copy <71971770+Copy05@users.noreply.github.com>
2023-10-11 11:39:58 +0200

    readme updated

---
+++ README.md
@@ -0,0 +1,11 @@
+# CopyrightArmor
+
+**CopyrightArmor** is a tool that scans the web for pirated content.
+
+# Installations
+
+**Install these dependencies**
+
+- requests
+- colorama
+- beautifulsoup4
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="CopyrightArmor",
+    version="0.0",
+    author="Copy05",
+    description="A tool that scans the web for pirated content",
+    url="https://github.com/Copy05/CopyrightArmor/",
+    install_requires=[
+        "beautifulsoup4",
+        "requests",
+        "colorama"
+    ],
+)
