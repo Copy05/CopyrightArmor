ccd753856c3f781b80be520bae608f89d51663fe
Copy <71971770+Copy05@users.noreply.github.com>
2024-01-15 11:03:42 +0100

    Report Generator

---
+++ report/create-report.py
@@ -0,0 +1,60 @@
+from func import *
+
+def main():
+
+    default_company = Load_Company()
+    agency = ""
+
+    ClearTerminal()
+    print("CopyrightArmor's DMCA Notice Report Creator.\n\n")
+
+    
+    print("How do you want your Company Name:")
+    what_company = CreateMenu([f"Use Default Company Name (\"{default_company}\")", "Enter it myself"])
+
+    if what_company == 1:
+        copyright_owner = default_company
+    elif what_company == 2:
+        copyright_owner = GetColoredInput("Enter The Copyright Owner")
+
+    if what_company == 2:
+        ClearTerminal()
+        print("Who does the claim:")
+        what_agency = CreateMenu(["On our own behalf", "We're an agency"])
+
+        if what_agency == 2:
+            agency = GetColoredInput("Enter The Agency Name")
+
+
+    ClearTerminal()
+    infringer = GetColoredInput("Enter The Infringer")
+
+    ClearTerminal()
+    product = GetColoredInput("Enter The stolen copyrighted product")
+
+    ClearTerminal()
+    what_letter = CreateMenu(["Use Template from settings.json", "Custom"])
+
+    if what_letter == 1:
+        letter = Load_Default_Letter()
+    elif what_letter == 2:
+        ClearTerminal()
+        CustomLetterContents(copyright_owner, infringer, product, agency)
+        letter = input()
+
+    ClearTerminal()
+    letter = FormatText(letter, copyright_owner, product, infringer, agency)
+
+    print(letter)
+    confirm = Confirmation("Do you want to save your letter?", "Save", "Don't Save")
+
+    if confirm == 1:
+        ClearTerminal()
+        default_filename = generate_unique_filename(f"{GetDate()}-{copyright_owner}", ".txt")
+        filename = input_with_default("Enter the filename", default_filename)
+        SaveReport(filename, letter)
+    elif confirm == 2:
+        exit()
+
+if __name__ == "__main__":
+    main()
---
+++ report/func.py
@@ -0,0 +1,170 @@
+import os
+import json
+
+from datetime import datetime
+from colorama import Fore, Back, Style, init
+
+def Load_Default_Letter():
+
+    try:
+        with open("settings.json", "r") as file:
+            settings_data = json.load(file)
+            letter_content = settings_data.get("LetterContent", "")
+            return letter_content
+    except FileNotFoundError:
+        print("Error: 'settings.json' file not found.")
+        return ""
+    except json.JSONDecodeError:
+        print("Error: Unable to parse 'settings.json'. Make sure it is a valid JSON file.")
+        return ""
+    
+    
+def Load_Company():
+
+    try:
+        with open("settings.json", "r") as file:
+            settings_data = json.load(file)
+            company = settings_data.get("CompanyName", "")
+            return company
+    except FileNotFoundError:
+        print("Error: 'settings.json' file not found.")
+        return ""
+    except json.JSONDecodeError:
+        print("Error: Unable to parse 'settings.json'. Make sure it is a valid JSON file.")
+        return ""
+    
+
+def Load_Agency():
+
+    try:
+        with open("settings.json", "r") as file:
+            settings_data = json.load(file)
+            agency = settings_data.get("AgencyName", "")
+            return agency
+    except FileNotFoundError:
+        print("Error: 'settings.json' file not found.")
+        return ""
+    except json.JSONDecodeError:
+        print("Error: Unable to parse 'settings.json'. Make sure it is a valid JSON file.")
+        return ""
+
+
+def GetDate():
+    return datetime.now().strftime("%Y-%m-%d")
+
+
+def CreateMenu(options):
+
+    for index, option in enumerate(options, start=1):
+        print(f"{index}. {option}")
+
+    while True:
+        try:
+            choice = int(input(f"{Fore.LIGHTYELLOW_EX}{Back.RESET}{Style.BRIGHT}Enter the number of your choice: {Style.RESET_ALL}{Fore.LIGHTCYAN_EX}"))
+            print(f"{Style.RESET_ALL}", end="")
+            if 1 <= choice <= len(options):
+                return choice
+            else:
+                print("Invalid choice. Please enter a valid number.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            
+
+def GetColoredInput(prompt):
+
+    init(autoreset=True)
+
+    colored_prompt = f"{Fore.LIGHTYELLOW_EX}{Back.RESET}{Style.BRIGHT}{prompt}: {Style.RESET_ALL}{Fore.LIGHTCYAN_EX}"
+
+    input_text = input(colored_prompt)
+
+    print(f"{Style.RESET_ALL}", end="")
+
+    return input_text
+
+
+def input_with_default(prompt, default):
+    user_input = input(f"{prompt} ({default}): ")
+    return user_input if user_input else default
+
+
+def generate_unique_filename(base_name, extension):
+    count = 1
+    file_name = f"{base_name}{extension}"
+
+    while os.path.exists(file_name):
+        count += 1
+        file_name = f"{base_name}-{count}{extension}"
+
+    return file_name
+
+
+def Confirmation(prompt, yes_text, no_text):
+    init(autoreset=True)
+    
+    while True:
+        response = input(f"{Fore.LIGHTYELLOW_EX}{Back.RESET}{Style.BRIGHT}{prompt}{Style.RESET_ALL}\n{Fore.LIGHTCYAN_EX}[Y] {yes_text}\t\t{Fore.LIGHTRED_EX}[N] {no_text}\n{Style.RESET_ALL}")
+        if response.lower() == 'y':
+            return True
+        elif response.lower() == 'n':
+            return False
+        else:
+            print("Invalid input. Please enter 'Y' or 'N'.")
+
+
+def SaveReport(file_name, letter_contents):
+    try:
+        base_name, extension = os.path.splitext(file_name)
+        count = 1
+
+        while os.path.exists(file_name):
+            count += 1
+            file_name = f"{base_name}-{count}{extension}"
+
+        file_path = os.path.join(os.getcwd(), file_name)
+
+        with open(file_path, "w") as file:
+            file.write(letter_contents)
+
+        print(f"Report saved successfully to: {file_path}")
+    except Exception as e:
+        print(f"Error saving report: {e}")
+
+
+def ClearTerminal():
+
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def FormatText(input_text, company, product, infringer, agency):
+
+    formatted_text = input_text.replace("{companyName}", company)
+    formatted_text = formatted_text.replace("{agencyName}", agency)
+    formatted_text = formatted_text.replace("{productName}", product)
+    formatted_text = formatted_text.replace("{infringerName}", infringer)
+    formatted_text = formatted_text.replace("\\n", "\n")
+
+    return formatted_text
+
+
+def CustomLetterContents(copyright_owner, infringer, product, agency=""):
+    
+    lt = '{'
+    rt = "}"
+
+    print(f"""
+Write the contents of the letter. the letter can contain placeholders that will be automatically converted to their properties.
+
+{Style.BRIGHT}SYNTAX{Style.RESET_ALL}:
+\t{Fore.LIGHTRED_EX}{lt}companyName{rt}{Style.RESET_ALL} - The Company who owns the Copyright.
+\t{Fore.LIGHTRED_EX}{lt}productName{rt}{Style.RESET_ALL} - The Product name.
+\t{Fore.LIGHTRED_EX}{lt}infringerName{rt}{Style.RESET_ALL} - The Name/Company of the Infringer.
+\t{Fore.LIGHTRED_EX}\\n{Style.RESET_ALL} - New Character.
+{f"{Fore.LIGHTRED_EX}{lt}agencyName{rt}{Style.RESET_ALL} - The DMCA Agency that works for the copyright owner." if agency else ""}
+
+{Style.BRIGHT}YOUR SUBMITTED DATA{Style.RESET_ALL}:
+\t{Fore.LIGHTYELLOW_EX}{Back.RESET}{Style.BRIGHT}Copyright Owner: {Style.RESET_ALL}{Fore.LIGHTCYAN_EX}{copyright_owner}{Style.RESET_ALL}
+\t{Fore.LIGHTYELLOW_EX}{Back.RESET}{Style.BRIGHT}Copyright Infringer: {Style.RESET_ALL}{Fore.LIGHTCYAN_EX}{infringer}{Style.RESET_ALL}
+\t{Fore.LIGHTYELLOW_EX}{Back.RESET}{Style.BRIGHT}Product: {Style.RESET_ALL}{Fore.LIGHTCYAN_EX}{product}{Style.RESET_ALL}
+{f"{Fore.LIGHTYELLOW_EX}{Back.RESET}{Style.BRIGHT}Agency Name: {Style.RESET_ALL}{Fore.LIGHTCYAN_EX}{agency}{Style.RESET_ALL}" if agency else ""}
+""")
