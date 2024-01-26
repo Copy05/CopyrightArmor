3ef0af8de9b1770e972a8df6ef50c429d251479c
Copy <71971770+Copy05@users.noreply.github.com>
2024-01-26 09:33:50 +0100

    refactoring

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup
 
 setup(
     name="CopyrightArmor",
-    version="0.1",
+    version="0.2",
     author="Copy05",
     description="A tool that scans the web for pirated content",
     url="https://github.com/Copy05/CopyrightArmor/",
